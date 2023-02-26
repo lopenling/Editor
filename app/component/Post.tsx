eec1a33f35a0da742e3f33ff50f0dc9df637fbce
kunsang <tenkus47@gmail.com>
2023-02-26 14:08:49 +0530

    added ! in error login

--- app/component/Post.tsx
+++ app/component/Post.tsx
@@ -102,7 +102,7 @@ const Posts = ({ postInfo, setPostInfo }: QuestionFormProps, ref: any) => {
             </div>
           </>
         ) : (
-          <div style={{ color: "red" }}>You must login first</div>
+          <div style={{ color: "red" }}>You must login first !</div>
         )}
       </createPost.Form>
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
