abc275f29d0cb4b0e9fe2c6bfa5a609d34ec0416
kunsang <tenkus47@gmail.com>
2023-03-09 13:18:56 +0530

    fix header responsive

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -12,7 +12,6 @@ export default function Header({ user }: any) {
   return (
     <div className=" max-w-full shadow-header px-2">
       <Navbar
-        rounded={true}
         fluid={false}
         className="mx-auto my-0 flex items-center"
         style={{
@@ -21,7 +20,7 @@ export default function Header({ user }: any) {
           maxWidth: MAX_WIDTH_PAGE,
         }}
       >
-        <NavLink preventScrollReset to={"/"} className="flex items-center">
+        <Navbar.Brand to={"/"} as={NavLink} className="flex items-center">
           <img
             src="https://lopenling.org/uploads/default/original/1X/0ac3db8e589f085c53c5ff8f36c17722888658ad.png"
             alt="logo"
@@ -34,98 +33,106 @@ export default function Header({ user }: any) {
             className="block md:hidden object-contain"
             style={{ maxHeight: 37 }}
           />
-        </NavLink>
-        <div className="flex">
-          <div className="flex items-center">
-            <Translation />
-          </div>
-          {user ? (
-            <div className="flex md:order-2">
-              <Dropdown
-                className="z-10"
-                inline={true}
-                arrowIcon={null}
-                label={
-                  <Avatar
-                    alt={user.name}
-                    img={user.avatarUrl}
-                    rounded={true}
-                    size="sm"
-                    title={user?.name}
-                  />
-                }
-              >
-                <Dropdown.Header>
-                  <span className="block truncate text-sm font-medium">
-                    {user?.email}
-                  </span>
-                </Dropdown.Header>
-                <Dropdown.Item
-                  className={user?.admin === "true" ? "" : "hidden"}
-                >
-                  <NavLink preventScrollReset to="/text-upload">
-                    UploadText
-                  </NavLink>
-                </Dropdown.Item>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <div
+            className={`flex ${
+              user ? "justify-between" : "flex-col"
+            } md:flex-row`}
+          >
+            <div className="flex items-center">
+              <Translation />
+            </div>
 
-                <Dropdown.Item>
-                  <Form
-                    method="post"
-                    action="/sso/login"
-                    className="flex items-center"
-                  >
-                    <input
-                      type="hidden"
-                      name="redirectTo"
-                      className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                      defaultValue={location.pathname}
+            {user ? (
+              <div className="flex md:order-2">
+                <Dropdown
+                  className="z-10"
+                  inline={true}
+                  arrowIcon={null}
+                  label={
+                    <Avatar
+                      alt={user.name}
+                      img={user.avatarUrl}
+                      rounded={true}
+                      size="sm"
+                      title={user?.name}
                     />
-                    <button
-                      className="text-sm font-medium leading-tight text-gray-900"
-                      type="submit"
-                      name="_action"
-                      value="logout"
-                    >
-                      {translation.logout}
-                    </button>
-                  </Form>
-                </Dropdown.Item>
-              </Dropdown>
-            </div>
-          ) : (
-            <div className="flex items-center gap-2">
-              <loginFetcher.Form
-                method="post"
-                action="/sso/login"
-                className="flex items-center"
-              >
-                <input
-                  type="hidden"
-                  name="redirectTo"
-                  defaultValue={location.pathname}
-                />
-
-                <button
-                  type="submit"
-                  name="_action"
-                  value="login"
-                  className="text-sm font-medium leading-tight text-gray-900 capitalize"
+                  }
                 >
-                  {translation.login}
-                </button>
-              </loginFetcher.Form>
-              <Button
-                // gradientDuoTone="tealToLime"
-                className=" text-green-400 border-2 border-green-300"
-                color=""
-              >
-                <a href={"https://lopenling.org/signup"}>
-                  {translation.signup}
-                </a>
-              </Button>
-            </div>
-          )}
-        </div>
+                  <Dropdown.Header>
+                    <span className="block truncate text-sm font-medium">
+                      {user?.email}
+                    </span>
+                  </Dropdown.Header>
+                  <Dropdown.Item
+                    className={user?.admin === "true" ? "" : "hidden"}
+                  >
+                    <NavLink preventScrollReset to="/text-upload">
+                      UploadText
+                    </NavLink>
+                  </Dropdown.Item>
+
+                  <Dropdown.Item>
+                    <Form
+                      method="post"
+                      action="/sso/login"
+                      className="flex items-center"
+                    >
+                      <input
+                        type="hidden"
+                        name="redirectTo"
+                        className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                        defaultValue={location.pathname}
+                      />
+                      <button
+                        className="text-sm font-medium leading-tight text-gray-900"
+                        type="submit"
+                        name="_action"
+                        value="logout"
+                      >
+                        {translation.logout}
+                      </button>
+                    </Form>
+                  </Dropdown.Item>
+                </Dropdown>
+              </div>
+            ) : (
+              <div className="flex gap-2 justify-between p-3">
+                <loginFetcher.Form
+                  method="post"
+                  action="/sso/login"
+                  className="flex items-center"
+                >
+                  <input
+                    type="hidden"
+                    name="redirectTo"
+                    defaultValue={location.pathname}
+                  />
+
+                  <button
+                    type="submit"
+                    name="_action"
+                    value="login"
+                    className="text-sm font-medium leading-tight text-gray-900 capitalize"
+                  >
+                    {translation.login}
+                  </button>
+                </loginFetcher.Form>
+                <Button
+                  // gradientDuoTone="tealToLime"
+                  className=" text-green-400 border-2 border-green-300"
+                  color=""
+                >
+                  <a href={"https://lopenling.org/signup"}>
+                    {translation.signup}
+                  </a>
+                </Button>
+              </div>
+            )}
+          </div>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
