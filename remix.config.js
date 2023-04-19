4a18f9d037a61aa6b10a961be25a8f85b048b012
kunsang <tenkus47@gmail.com>
2023-04-19 12:25:22 +0530

    add _redirects

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,7 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /** @type {import('@remix-run/dev').AppConfig} */
+
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   server:
@@ -9,10 +12,9 @@ module.exports = {
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
-  // future: {
-  //   v2_errorBoundary: true,
-  //   v2_meta: true,
-  //   v2_normalizeFormMethod: true,
-  //   v2_routeConvention: true,
-  // },
+
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
 };
