32ef0e9f44212741fe7fab5d6103f42e576e53a9
kunsang <tenkus47@gmail.com>
2023-05-08 19:06:42 +0530

    removed strict

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -9,10 +9,5 @@ import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import React from "react";
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <React.StrictMode>
-      <RemixBrowser />
-    </React.StrictMode>
-  );
+  hydrateRoot(document, <RemixBrowser />);
 });
