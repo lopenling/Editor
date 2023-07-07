d7c6712c488e085939f58e75ba0f73501e562c1f
kunsang <tenkus47@gmail.com>
2023-07-07 14:48:14 +0530

    config update

--- remix.config.js
+++ remix.config.js
@@ -4,12 +4,8 @@ const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
 
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.ts' : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
   serverModuleFormat: 'cjs',
-  // appDirectory: "app",
   assetsBuildDirectory: 'public/build',
-  // publicPath: "/build/",
   future: {
     v2_routeConvention: true,
     v2_errorBoundary: true,
