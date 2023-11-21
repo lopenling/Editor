11cb5cdc690d85f273bb782cdb2b6a31f6315e87
kunsang <tenkus47@gmail.com>
2023-11-21 13:55:25 +0530

    add scrolling of h1

--- app/routes/text_.$textId.page.$pageId.translation.$translationId.tsx
+++ app/routes/text_.$textId.page.$pageId.translation.$translationId.tsx
@@ -11,6 +11,7 @@ import { AiFillSave } from 'react-icons/ai';
 import { Button } from 'flowbite-react';
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { db } from '~/services/db.server';
+import { useEffect, useRef, useState } from 'react';
 export const loader: LoaderFunction = async ({ request, params }) => {
   let textId = params.textId;
   let order = params.pageId;
@@ -61,6 +62,10 @@ function TranslationsRoute() {
   let { translation, userText, textId, order } = useLoaderData();
   let source_editor = useEditorInstance(userText.content, true, false);
   let translation_editor = useEditorInstance(translation.content, true, false);
+
+  let sourceRef = useRef<HTMLDivElement>(null);
+  let translationRef = useRef<HTMLDivElement>(null);
+
   let fetcher = useFetcher();
   function save() {
     fetcher.submit(
@@ -78,6 +83,60 @@ function TranslationsRoute() {
   function share() {
     navigator.clipboard.writeText(window.location.href);
     alert('text url copied');
+  }
+  const [prevVisibleIndex, setPrevVisibleIndex] = useState(null);
+  const [prevVisibleElement] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = (element: HTMLDivElement) => {
+      const h1Elements = element.querySelectorAll('h1');
+
+      // Find the index and element of the first visible h1 element
+      const firstVisibleH1 = Array.from(h1Elements).find((h1) => {
+        const h1Rect = h1.getBoundingClientRect();
+        const parentRect = element?.getBoundingClientRect();
+
+        // Check if the h1 element is fully or partially visible within its parent div
+        return h1Rect.top >= parentRect.top && h1Rect.bottom <= parentRect.bottom;
+      });
+
+      // Log the index and element of the first visible h1 if they change
+      if (firstVisibleH1) {
+        const firstVisibleIndex = Array.from(h1Elements).indexOf(firstVisibleH1);
+
+        if (firstVisibleIndex !== prevVisibleIndex || firstVisibleH1 !== prevVisibleElement) {
+          setCurrentIndex(firstVisibleIndex);
+          // Update the previous values
+          setPrevVisibleIndex(firstVisibleIndex);
+        }
+      }
+    };
+
+    // Add scroll event listener to the sourceRef
+    sourceRef.current?.addEventListener('scroll', () => handleScroll(sourceRef.current));
+    translationRef.current?.addEventListener('scroll', () => handleScroll(translationRef.current));
+    // Clean up the event listener on component unmount
+    return () => {
+      sourceRef.current?.removeEventListener('scroll', () => handleScroll(sourceRef.current));
+      translationRef.current?.removeEventListener('scroll', () => handleScroll(translationRef.current));
+    };
+  }, [prevVisibleIndex]);
+
+  useEffect(() => {
+    let transElement = translationRef.current?.querySelectorAll('h1')[currentIndex];
+    let sourceElement = sourceRef.current?.querySelectorAll('h1')[currentIndex];
+
+    if (translationRef.current !== currentDiv.current) {
+      transElement?.scrollIntoView({ block: 'center' });
+    }
+    if (sourceRef.current !== currentDiv.current) {
+      sourceElement?.scrollIntoView({ block: 'center' });
+    }
+  }, [currentIndex]);
+  function handleChangeCurrentDiv(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    currentDiv.current = e.currentTarget;
   }
   return (
     <>
@@ -102,12 +161,20 @@ function TranslationsRoute() {
         </div>
       </div>
       <div className="flex max-w-6xl gap-3 w-full mx-auto mt-3">
-        <div className="flex-1 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-h-[80vh] overflow-y-scroll">
+        <div
+          ref={sourceRef}
+          onMouseOver={handleChangeCurrentDiv}
+          className="flex-1 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-h-[80vh] overflow-y-scroll"
+        >
           <h2 className="text-gray-400">Source Text</h2>
           <Tools editor={source_editor} />
           <EditorContent editor={source_editor} />
         </div>
-        <div className="flex-1 block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-h-[80vh] overflow-y-scroll">
+        <div
+          ref={translationRef}
+          onMouseOver={handleChangeCurrentDiv}
+          className="flex-1 block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-h-[80vh] overflow-y-scroll"
+        >
           <h2 className="text-gray-400">Translation Text</h2>
           <Tools editor={translation_editor} />
           <EditorContent editor={translation_editor} />
