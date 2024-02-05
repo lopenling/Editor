7aeba2861c9dbd4d5b16da3829177479d5c10229
kunsang <tenkus47@gmail.com>
2024-02-05 15:40:20 +0530

    added intext search location

--- app/component/menu/Search.tsx
+++ app/component/menu/Search.tsx
@@ -5,8 +5,9 @@ import { SearchString } from '~/features/Editor';
 function Search({ editor }: { editor: Editor | null }) {
   if (!editor) return null;
   return (
-    <div>
+    <div className="flex flex-col flex-1">
       <SearchString editor={editor} />
+      <div className="bg-green-200 flex-1 h-full">intext</div>
     </div>
   );
 }
